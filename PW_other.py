f7b37cc2fdf0d843462ea615db347f33da951b11
Tash-Tim <tnazarov1991@gmail.com>
2025-03-05 14:39:16 +0500

    Add files via upload

---
+++ PW_other.py
@@ -0,0 +1,36 @@
+# text = 'Привет! меня зовут Темур'
+# temp_list = ''
+# text_list = []
+#
+# # формирует список со вложенным списком
+# for i in text + ' ':
+#     if i != ' ':
+#         temp_list += i
+#     else:
+#         text_list.append(list(temp_list))
+#         temp_list = ''
+#
+# print(text_list)
+#
+# # выводит на экран список в виде матрицы
+# for i_list in text_list:
+#     for j in i_list:
+#         print(j, end = '')
+#     print()
+
+
+"""
+не удалять верхний код
+"""
+
+# rev_dbl_num = [num * 2 for num in range(10 , 1 , -1) if num % 2 == 0]
+# print(rev_dbl_num)
+
+# words = ['hello', 'hey', 'goodbye', 'guitar', 'piano']
+# new_words = [word + '.' for word in words if len(word) <= 5]
+# print(new_words)
+
+# a = [1, 4, 6, 10, 2, 3]
+# a[:5] = [1, 2, 3]
+# print(a)
+
